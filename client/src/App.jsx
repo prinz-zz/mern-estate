dcd10bafccfe95c27237f72a2b5842823345e8cd
Prinz Adamz <prinzadamz@gmail.com>
2023-11-12 15:38:43 +0530

    Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className='text-red-800' >App</h1>
+      <h1 className="text-red-800">App</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/SignOut.jsx
@@ -0,0 +1,7 @@
+
+
+export default function SignOut() {
+    return (
+        <div className=''>SignOut</div>
+    )
+}
