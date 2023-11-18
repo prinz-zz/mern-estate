cdb303843377446dd94af3b47c03b5a74e13e28d
Prinz Adamz <prinzadamz@gmail.com>
2023-11-18 11:58:29 +0530

    Create user listings API

--- api/controller/userController.js
+++ api/controller/userController.js
@@ -1,6 +1,7 @@
 import { errorMessage } from "../utils/error.js";
 import bcrypt from "bcrypt";
 import User from "../models/userModel.js";
+import Listing from "../models/listingModel.js";
 
 ///////////////UPDATE USER
 export const updateUser = async (req, res, next) => {
@@ -39,13 +40,24 @@ export const deleteUser = async (req, res, next) => {
   }
 
   try {
-    await User.findByIdAndDelete(req.params.id,)
-    res.clearCookie('jwt_access')
-    res.status(200).json('User deleted successfully')
+    await User.findByIdAndDelete(req.params.id);
+    res.clearCookie("jwt_access");
+    res.status(200).json("User deleted successfully");
   } catch (error) {
-    next(error)
+    next(error);
   }
-
 };
 
-
+////////////////////////////getUserListings/////////////////
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorMessage(401, "You can only view your own listings "));
+  }
+};
--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 const router = express.Router();
 
-import { updateUser, deleteUser } from '../controller/userController.js';
+import { updateUser, deleteUser, getUserListings } from '../controller/userController.js';
 import { verifyToken } from '../utils/verifyToken.js';
 
 router.put("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listings/:id", verifyToken, getUserListings);
 
 
 export default router;
